ae7f8bbc98c27f0d64f886d2b1821637c0a0e9ea
shutech2001 <shutech2001@outlook.jp>
2020-10-02 06:30:04 +0000

    Initial commit

---
+++ add5.s
@@ -0,0 +1,23 @@
+	.file	"add5.c"
+	.text
+	.globl	add5
+	.type	add5, @function
+add5:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	-4(%rbp), %eax
+	addl	$5, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	add5, .-add5
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
